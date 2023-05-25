b0fa5625f5128447569212b557a34316e0116c9e
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2023-05-25 22:19:28 +0200

    Namespaces
    Start to splitting files into smaller one. Then wrapping it in namespaces.

---
+++ prj-00-initial-starting-setup/src/drag-drop-interfaces.ts
@@ -0,0 +1,15 @@
+// Drag & Drop Interfaces
+// only TS feature; any code can be inside; it is not available outside of this file
+namespace App {
+  // export keyword allow code to be exported outside of namespace
+  export interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHandler(event: DragEvent): void;
+  }
+
+  export interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+  }
+}
