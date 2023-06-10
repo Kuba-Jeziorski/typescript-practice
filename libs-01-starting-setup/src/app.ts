072de9baf2d5776bdf4d22e2f64d047a3f131152
Jakub Jeziorski <106917056+Kuba-Jeziorski@users.noreply.github.com>
2023-06-10 19:33:38 +0200

    ts - 13 section - finished

--- libs-01-starting-setup/src/app.ts
+++ libs-01-starting-setup/src/app.ts
@@ -1,3 +1,24 @@
-import _ from "lodash";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
 
-console.log(_.shuffle([1, 2, 3]));
+import { Product } from "./product.model";
+
+const products = [
+  { title: "A carpet", price: 29.99 },
+  { title: "A book", price: 10.99 },
+];
+// const p1 = new Product("A Book", 12.99);
+
+// vanilla ts solution
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+// 3rd party library; class-transformer
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
+
+// console.log(p1.getInformation());
